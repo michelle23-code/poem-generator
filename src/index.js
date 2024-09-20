3e1a96a82a9c6d4189138f17c0d440f4c682169b
michelle23-code <michellelekoto13@icloud.com>
2024-09-20 12:37:25 +0200

    Removed all the concole logs

--- src/index.js
+++ src/index.js
@@ -14,10 +14,13 @@ function generatePoem(event) {
   let apiKey = "13564a02toa98fc1f3feb5454b70d3af";
   let prompt = `Generate a poem about ${instructionsInput.value}`;
   let context =
-    "You are English  teacher and love to write short poems.You mission is to generate 6 line poems in basic HTML.please hind the word html inside the poem .The poem should have a heading.please sigh the the poem with AI Generated poem at the end with<strong>.Make sure to follow the user instructins";
+    "You are English  teacher and love to write short poems.You mission is to generate 6 line poems in basic HTML.The poem should have a heading and please highligh this in plum.please tell the user to feel to copy the poem ❤️.please sigh the the poem with AI Generated poem at the end with<strong>.Make sure to follow the user instructins";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Genarating a poem");
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ please wait the AI is generating about ${instructionsInput.value}</div>`;
+  
   axios.get(apiUrl).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-generator");
